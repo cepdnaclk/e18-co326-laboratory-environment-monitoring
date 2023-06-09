a6196f4372271e4d272d91031158cc897f8d89e6
Chathura Thimanka Wimalasiri <73394733+chathurawimalasiri@users.noreply.github.com>
2023-06-09 12:57:26 +0530

    Finished backend without mqtt part (#3)
    * finished backend without mqtt part
    
    * Add the User.js

---
+++ code/backend/app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const mqtt_listener = require('./mqtt_listener');
+const { mongoose } = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+
+const port = 3001;
+const app = express();
+
+dotenv.config(); // Load environment variables
+
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use(require("./routes/deviceRoutes"));
+app.use(require("./routes/userRoutes"));
+app.use(require("./routes/stationRoutes"));
+
+// Start server
+app.listen(port, async () => {
+  
+  const mongoDB = process.env.MONGODB_URI;
+  mongoose.set("strictQuery", true);
+  await mongoose
+    .connect(mongoDB, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("Connected to MongoDB");
+      mqtt_listener.run();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+  console.log(`Server is running on port: ${port}`);
+});
+
+module.exports = app;
